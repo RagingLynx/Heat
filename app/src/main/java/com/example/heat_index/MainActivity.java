9a7dee259f55f1220bcbbf50e4c71047cf4e6eac
Kevin Heinz <kevin1.heinz@gmail.com>
2019-10-17 22:05:12 +0200

    minor fixes

--- app/src/main/java/com/example/heat_index/MainActivity.java
+++ app/src/main/java/com/example/heat_index/MainActivity.java
@@ -1,57 +1,49 @@
 package com.example.heat_index;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
 
 //Noch die Main, sollte aber ausgetauscht werden sobald alle anderen laufen
 public class MainActivity extends AppCompatActivity {
     private Button switchDegree;
     private boolean isFahrenheit = false;
-    private Button calculate;
     private EditText temp_text;
     private EditText humidity;
-    private Button switchbutton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
-
         temp_text = findViewById(R.id.temp_text);
         humidity = findViewById(R.id.humidity_text);
-        switchbutton = findViewById(R.id.calculate);
+        Button calculateButton = findViewById(R.id.calculate);
 
-        switchDegree = (Button) findViewById(R.id.degree_switch);
+        switchDegree = findViewById(R.id.degree_switch);
         switchDegree.setOnClickListener(e -> {
             if(!isFahrenheit) {
-                switchDegree.setText("°F");
+                switchDegree.setText(getString(R.string.f));
             }
             else{
-                switchDegree.setText("°C");
+                switchDegree.setText(getString(R.string.c));
             }
             isFahrenheit = !isFahrenheit;
         });
 
-        calculate = findViewById(R.id.calculate);
-        calculate.setOnClickListener(e -> {
+
+
+        calculateButton.setOnClickListener(e -> {
+            Intent intent = new Intent(this, EingabeActivity.class);
+            startActivity(intent);
             Weather w = new Weather(Double.parseDouble(temp_text.getText().toString()), Double.parseDouble(humidity.getText().toString()), isFahrenheit);
             System.out.println(Double.parseDouble(temp_text.getText().toString()));
             System.out.println(Double.parseDouble(humidity.getText().toString()));
             System.out.println(isFahrenheit);
             System.out.println(w.getHeatIndex());
-        });
-
-        switchbutton.setOnClickListener(e -> {
-            Intent intent = new Intent(this, EingabeActivity.class);
-            startActivity(intent);
 
         });
 
