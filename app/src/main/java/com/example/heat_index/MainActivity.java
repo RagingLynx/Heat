fe96692cedf388b1ff17eb967a096e8a033377f3
Kevin Heinz <kevin1.heinz@gmail.com>
2019-10-18 21:53:01 +0200

    Titel der Actionbar ändert sich mit dem ausgewählten Fragment

--- app/src/main/java/com/example/heat_index/MainActivity.java
+++ app/src/main/java/com/example/heat_index/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,
                 eingabeFrag).commit();
+        setTitle(R.string.heat_index_berechnen);
         BottomNavigationView botNav = findViewById(R.id.bottom_bar);
         botNav.setOnNavigationItemSelectedListener(navbarListener);
 
@@ -52,15 +53,19 @@ public class MainActivity extends AppCompatActivity {
                 switch (menuItem.getItemId()) {
                     case R.id.eingabe_nav:
                         selectedFrag = eingabeFrag;
+                        setTitle(R.string.heat_index_berechnen);
                         break;
                     case R.id.ausgabe_nav:
                         selectedFrag = ausgabeFrag;
+                        setTitle(R.string.ergebnis_title);
                         break;
                     case R.id.verlauf_nav:
                         selectedFrag = verlaufFrag;
+                        setTitle(R.string.verlauf_title);
                         break;
                     case R.id.info_nav:
                         selectedFrag = infoFrag;
+                        setTitle(R.string.info_title);
                         break;
                 }
 
