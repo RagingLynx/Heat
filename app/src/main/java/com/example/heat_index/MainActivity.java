8f7f1d4cc4fada64ed9992d48caa6db3d2a329eb
Kevin Heinz <kevin1.heinz@gmail.com>
2019-10-18 19:18:43 +0200

    Fragments eingefügt BottomBar fast funktionsfähig

--- app/src/main/java/com/example/heat_index/MainActivity.java
+++ app/src/main/java/com/example/heat_index/MainActivity.java
@@ -2,51 +2,60 @@ package com.example.heat_index;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 //Noch die Main, sollte aber ausgetauscht werden sobald alle anderen laufen
 public class MainActivity extends AppCompatActivity {
-    private Button switchDegree;
-    private boolean isFahrenheit = false;
-    private EditText temp_text;
-    private EditText humidity;
+//    private Button switchDegree;
+//    private boolean isFahrenheit = false;
+//    private EditText temp_text;
+//    private EditText humidity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first);
+        setContentView(R.layout.activity_main);
 
-        temp_text = findViewById(R.id.temp_text);
-        humidity = findViewById(R.id.humidity_text);
-        Button calculateButton = findViewById(R.id.calculate);
-
-        switchDegree = findViewById(R.id.degree_switch);
-        switchDegree.setOnClickListener(e -> {
-            if(!isFahrenheit) {
-                switchDegree.setText(getString(R.string.f));
-            }
-            else{
-                switchDegree.setText(getString(R.string.c));
-            }
-            isFahrenheit = !isFahrenheit;
-        });
-
-
-
-        calculateButton.setOnClickListener(e -> {
-            Intent intent = new Intent(this, EingabeActivity.class);
-            startActivity(intent);
-            Weather w = new Weather(Double.parseDouble(temp_text.getText().toString()), Double.parseDouble(humidity.getText().toString()), isFahrenheit);
-            System.out.println(Double.parseDouble(temp_text.getText().toString()));
-            System.out.println(Double.parseDouble(humidity.getText().toString()));
-            System.out.println(isFahrenheit);
-            System.out.println(w.getHeatIndex());
-
-        });
-
+        BottomNavigationView botNav = findViewById(R.id.bottom_bar);
+        botNav.setOnNavigationItemSelectedListener(navbarListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,
+                new EingabeFragment()).commit();
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener navbarListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFrag = null;
+                    switch (menuItem.getItemId()) {
+                        case R.id.eingabe_nav:
+                            selectedFrag = new EingabeFragment();
+                            break;
+                        case R.id.ausgabe_nav:
+                            selectedFrag = new AusgabeFragment();
+                            break;
+                        case R.id.verlauf_nav:
+                            selectedFrag = new VerlaufFragment();
+                            break;
+                        case R.id.info_nav:
+                            selectedFrag = new InfoFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,
+                            selectedFrag).commit();
+                    return true;
+                }
+    };
+
 }
+
