dcaf3a713e8c1d81c0d07e3cd66aebfc6736b068
Kevin Heinz <kevin1.heinz@gmail.com>
2019-10-18 21:54:01 +0200

    ungenutze imports und variablen aus der MainActivity geschmissen

--- app/src/main/java/com/example/heat_index/MainActivity.java
+++ app/src/main/java/com/example/heat_index/MainActivity.java
@@ -1,27 +1,16 @@
 package com.example.heat_index;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.FrameLayout;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 //Noch die Main, sollte aber ausgetauscht werden sobald alle anderen laufen
 public class MainActivity extends AppCompatActivity {
-    private Button switchDegree;
-    private boolean isFahrenheit = false;
-    private EditText temp_text;
-    private EditText humidity;
+
     private FrameLayout frameContainer;
     private Fragment eingabeFrag, ausgabeFrag, infoFrag, verlaufFrag;
 
